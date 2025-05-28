40b5ff701dbe53b59efde1546586ed91850042c6
zsc <2827636950@qq.com>
2025-05-28 16:19:15 +0800

    更新了头像和页面标题的图标

--- github_myprofile_updater/update.py
+++ github_myprofile_updater/update.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     _intro = open(f'{base_dir}/intro.md').read().strip()
     _homepage = open(f'{base_dir}/homepage.md').read().strip()
     _pub = open(f'{base_dir}/pub_short.md').read().strip()
-    _news = open(f'{base_dir}/news.md').read().strip()
+    _news = open(f'{base_dir}/education.md').read().strip()
     with open('README.md', 'w') as f:
         f.write(_header)
         f.write('\n\n')
